3e4ce89da9c028efa8cd3bbdc5db75b570fdd668
imsk17 <imsumitkr17@gmail.com>
2024-02-27 21:27:49 +0530

    Types: Mint and Deploy use Property Syntax

--- src/handlers/types/chain.ts
+++ src/handlers/types/chain.ts
@@ -2,15 +2,15 @@ import { BridgeStorage } from "../../contractsTypes/evm";
 import { TSupportedChain, TSupportedSftChain } from "../../factory/types/utils";
 
 export type MintNft<Signer, MintArgs, RetTx> = {
-  mintNft(signer: Signer, ma: MintArgs): Promise<RetTx>;
+  mintNft: (signer: Signer, ma: MintArgs) => Promise<RetTx>;
 };
 
 export type DeployCollection<Signer, DeployArgs, GasArgs, RetTx> = {
-  deployCollection(
+  deployCollection: (
     signer: Signer,
     da: DeployArgs,
     ga: GasArgs | undefined,
-  ): Promise<RetTx>;
+  ) => Promise<RetTx>;
 };
 
 /**
