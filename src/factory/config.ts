df9c06d1f5369f89eb68a374e8f29fcebc8c4d6a
imsk17 <imsumitkr17@gmail.com>
2024-02-27 21:31:50 +0530

    Chains: Testnet: Set Royalty Sale Price to 10000

--- src/factory/config.ts
+++ src/factory/config.ts
@@ -35,14 +35,14 @@ export namespace ChainFactoryConfigs {
         identifier: "BSC",
         provider: new JsonRpcProvider("https://bsc-testnet.publicnode.com"),
         bridge: ethers.getAddress("0x3EC2839EcEAfa2Ce9e419718364B070563Db516e"),
-        royaltySalePrice: 0,
+        royaltySalePrice: 10000,
         storage,
       },
       ethParams: {
         identifier: "ETH",
         provider: new JsonRpcProvider("https://rpc.notadegen.com/eth/sepolia"),
         bridge: ethers.getAddress("0xA0C15702892491597D369A04d7366Af234cF5F73"),
-        royaltySalePrice: 0,
+        royaltySalePrice: 10000,
         storage,
       },
       polygonParams: {
@@ -51,14 +51,14 @@ export namespace ChainFactoryConfigs {
           "https://polygon-mumbai-bor.publicnode.com",
         ),
         bridge: ethers.getAddress("0x1f89dce7eBFb78D6CA0A4d16F9B6a9F61B186ebc"),
-        royaltySalePrice: 0,
+        royaltySalePrice: 10000,
         storage,
       },
       hederaParams: {
         bridge: ethers.getAddress("0x742f68F53583Be22a62338e916eB8DE90D07daB1"),
         identifier: "HEDERA",
         provider: new JsonRpcProvider("https://testnet.hashio.io/api"),
-        royaltySalePrice: 0,
+        royaltySalePrice: 10000,
         storage,
       },
       tezosParams: {
