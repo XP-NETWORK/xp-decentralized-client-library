5d50ab7ee764b566fb547d70ac61ede8aa633c1f
imsk17 <imsumitkr17@gmail.com>
2024-02-28 13:33:48 +0000

    Build: (02cf101) Secret: Import SecretJS before Wallet

--- dist/handlers/secret/index.d.ts
+++ dist/handlers/secret/index.d.ts
@@ -1,3 +1,4 @@
+import "secretjs";
 import { TSecretHandler, TSecretParams } from "./types";
 export declare function secretHandler({ bridge, provider, storage, bridgeCodeHash, }: TSecretParams): TSecretHandler;
 //# sourceMappingURL=index.d.ts.map
