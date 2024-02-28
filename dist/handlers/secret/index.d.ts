0a1c042d7d7fa1dc0a1591e9a81291c237573001
imsk17 <imsumitkr17@gmail.com>
2024-02-28 14:00:46 +0000

    Build: (42e47a2) Secret: Fix SecretJS Wallet Issue

--- dist/handlers/secret/index.d.ts
+++ dist/handlers/secret/index.d.ts
@@ -1,4 +1,7 @@
-import "secretjs";
+import { StdSignature } from "secretjs";
+import { Pubkey } from "secretjs/dist/wallet_amino";
 import { TSecretHandler, TSecretParams } from "./types";
 export declare function secretHandler({ bridge, provider, storage, bridgeCodeHash, }: TSecretParams): TSecretHandler;
+export declare function encodeSecp256k1Signature(pubkey: Uint8Array, signature: Uint8Array): StdSignature;
+export declare function encodeSecp256k1Pubkey(pubkey: Uint8Array): Pubkey;
 //# sourceMappingURL=index.d.ts.map
