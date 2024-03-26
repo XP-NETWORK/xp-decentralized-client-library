907a63e6bcf2e1c2c907854749b4c127cd363899
imsk17 <imsumitkr17@gmail.com>
2024-03-26 17:58:19 +0000

    Build: (bbac5e6) Cosmos: Setup Handler

---
+++ dist/handlers/comos/index.d.ts
@@ -0,0 +1,3 @@
+import { TCosmosHandler, TCosmosParams } from "./types";
+export declare function cosmosHandler({ provider, rpc, bridge, storage, }: TCosmosParams): TCosmosHandler;
+//# sourceMappingURL=index.d.ts.map
