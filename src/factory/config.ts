3113348b1fdd2a5327da4591482a7d3f0bafd5c0
imsk17 <imsumitkr17@gmail.com>
2024-05-03 15:52:48 +0530

    Testnet: Secret: Config: Update Bridge Contracts

--- src/factory/config.ts
+++ src/factory/config.ts
@@ -70,7 +70,7 @@ export namespace ChainFactoryConfigs {
         tzktApi: "https://api.ghostnet.tzkt.io/",
       },
       secretParams: {
-        bridge: "secret1uxthnjt74cny9wnwx8czmgrc8taz0r8yfas565",
+        bridge: "secret1c9k79ee6t0erdawjv8nflgeynt70ryke26vj7d",
         bridgeCodeHash: "",
         chainId: "pulsar-3",
         provider: new SecretNetworkClient({
