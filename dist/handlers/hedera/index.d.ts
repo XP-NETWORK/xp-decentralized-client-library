d5866243ef33b154e901f75176a3d8e4ec44f8c7
imsk17 <imsumitkr17@gmail.com>
2024-07-11 09:55:36 +0000

    Build: (4d94c76) Hedera: Adapt for Hashpack and Metamask both

--- dist/handlers/hedera/index.d.ts
+++ dist/handlers/hedera/index.d.ts
@@ -1,3 +1,6 @@
+import { Signer as HederaSigner } from "@hashgraph/sdk";
+import { Signer } from "ethers";
 import { THederaHandler, THederaParams } from "./types";
 export declare function hederaHandler({ provider, royaltySalePrice, royaltyProxy, storage, identifier, bridge, }: THederaParams): THederaHandler;
+export declare function isHederaSigner(signer: HederaSigner | Signer): signer is HederaSigner;
 //# sourceMappingURL=index.d.ts.map
--- dist/handlers/hedera/types.d.ts
+++ dist/handlers/hedera/types.d.ts
@@ -1,16 +1,17 @@
+import { Signer as HederaSigner } from "@hashgraph/sdk";
 import { ContractTransactionResponse, Overrides, Provider, Signer } from "ethers";
 import { Bridge } from "../../contractsTypes/evm";
 import { PayableOverrides } from "../../contractsTypes/evm/common";
 import { TEvmParams } from "../evm/types";
-import { DeployCollection, MintNft, ReadClaimed721Event, ReadClaimed1155Event, TApproveNFT, TNftChain } from "../types";
+import { DeployCollection, MintNft, ReadClaimed721Event, TApproveNFT, TSingularNftChain } from "../types";
 export type HederaMintArgs = {
     contract: string;
     uri: string;
 };
-export type THederaHandler = TNftChain<Signer, Bridge.ClaimDataStruct, Overrides, ContractTransactionResponse, Provider> & MintNft<Signer, HederaMintArgs, PayableOverrides, ContractTransactionResponse> & TApproveNFT<Signer, PayableOverrides, ContractTransactionResponse> & DeployCollection<Signer, {
+export type THederaHandler = TSingularNftChain<Signer | HederaSigner, Bridge.ClaimDataStruct, Overrides, ContractTransactionResponse, Provider> & MintNft<Signer, HederaMintArgs, PayableOverrides, ContractTransactionResponse> & TApproveNFT<Signer, PayableOverrides, ContractTransactionResponse> & DeployCollection<Signer, {
     name: string;
     symbol: string;
-}, Overrides, string> & ReadClaimed721Event & ReadClaimed1155Event;
+}, Overrides, string> & ReadClaimed721Event;
 export type THederaParams = TEvmParams & {
     royaltySalePrice: number;
     royaltyProxy: string;
