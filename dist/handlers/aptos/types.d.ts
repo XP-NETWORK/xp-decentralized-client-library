05346790de97669c69528cc3b357582aaaab55f6
imsk17 <imsumitkr17@gmail.com>
2024-07-19 09:44:43 +0000

    Build: (10cf284) Aptos: Implement all methods

--- dist/handlers/aptos/types.d.ts
+++ dist/handlers/aptos/types.d.ts
@@ -1,9 +1,17 @@
 import { Account, Aptos, PendingTransactionResponse } from "@aptos-labs/ts-sdk";
 import { Network } from "aptos";
 import { BridgeStorage } from "../../contractsTypes/evm";
-import { TNftChain } from "../types";
+import { DeployCollection, MintNft, TNftChain } from "../types";
 import { TClaimData } from "./bridge-client";
-export type TAptosHandler = TNftChain<Account, TClaimData, never, PendingTransactionResponse, Aptos>;
+export type TAptosMintArgs = {
+    contract: string;
+    name: string;
+    uri: string;
+};
+export type TAptosHandler = TNftChain<Account, TClaimData, never, PendingTransactionResponse, Aptos> & MintNft<Account, TAptosMintArgs, never, string> & DeployCollection<Account, {
+    name: string;
+    symbol: string;
+}, never, string>;
 export type TAptosParams = {
     bridge: string;
     network: Network;
