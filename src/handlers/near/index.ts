c37296f8f4466f3e2f5d77cf1ef3b14574d65fc1
imsk17 <imsumitkr17@gmail.com>
2024-09-11 19:57:58 +0530

    feat(near): implement handler

---
+++ src/handlers/near/index.ts
@@ -0,0 +1,227 @@
+import { Contract } from "near-api-js";
+import { NEAR_NOMINATION } from "near-api-js/lib/utils/format";
+import { unimplemented } from "../../utils";
+import { TNFTData } from "../types";
+import { fetchHttpOrIpfs } from "../utils";
+import { TNearHandler, TNearParams } from "./types";
+
+export function multiversxHandler({
+  provider,
+  bridge,
+  storage,
+  identifier,
+}: TNearParams): TNearHandler {
+  const bc = new Contract(provider.connection, bridge, {
+    changeMethods: ["lock_nft", "claim_nft"],
+    useLocalViewExecution: false,
+    viewMethods: ["validator_count"],
+  });
+  return {
+    async nftData(nonce, collection) {
+      const contract = new Contract(provider.connection, collection, {
+        viewMethods: ["nft_token", "nft_metadata"],
+        changeMethods: [],
+        useLocalViewExecution: false,
+      });
+      //@ts-ignore ik it works.
+      const nft_metadata = await contract.nft_token({ token_id: nonce });
+      //@ts-ignore ik it works.
+      const collection_metadata = await contract.nft_metadata();
+      return {
+        name: nft_metadata.metadata.title,
+        symbol: collection_metadata.symbol,
+        metadata: nft_metadata.metadata.media || nft_metadata.metadata.extra,
+        royalty: BigInt(
+          Object.values(nft_metadata.metadata.royalty).reduce(
+            //@ts-ignore ik it works
+            (e: number, c: number) => c + e,
+          ) as number,
+        ),
+      };
+    },
+    async deployNftCollection(_signer, _da, _ga) {
+      unimplemented();
+    },
+    getProvider() {
+      return provider;
+    },
+    async readClaimed721Event(hash) {
+      const receipts = await provider.connection.provider.txStatusReceipts(
+        hash,
+        bridge,
+        "FINAL",
+      );
+      const log = receipts.receipts_outcome
+        .map((e) => e.outcome.logs)
+        .flatMap((e) => e)
+        .filter((e) => e.includes("claimed"))[0];
+      if (!log) throw new Error("No Claimed Event Found");
+      const parsed = JSON.parse(log);
+      return {
+        transaction_hash: parsed.data.transaction_hash,
+        nft_contract: parsed.data.contract,
+        source_chain: parsed.data.source_chain,
+        token_id: parsed.data.token_id,
+        lock_tx_chain: parsed.data.lock_tx_chain,
+      };
+    },
+    async getValidatorCount() {
+      //@ts-ignore ik it works
+      return bc.validator_count();
+    },
+    getStorageContract() {
+      return storage;
+    },
+    async mintNft(signer, ma, gasArgs) {
+      const call = await signer.functionCall({
+        contractId: ma.contract,
+        methodName: "nft_mint",
+        args: {
+          token_id: ma.token_id,
+          receiver_id: ma.owner,
+          metadata: {
+            title: ma.title || "Xp Nft",
+            description: ma.description || "Xp Nft Description",
+            media: ma.uri,
+          },
+        },
+        attachedDeposit: NEAR_NOMINATION / 1000n,
+        gas: gasArgs ? BigInt(gasArgs.gasLimit) : undefined,
+      });
+      return call.transaction.hash;
+    },
+    transform(input) {
+      return {
+        destination_chain: input.destinationChain,
+        destination_user_address: input.destinationUserAddress,
+        fee: input.fee,
+        metadata: input.metadata,
+        name: input.name,
+        nft_type: input.nftType,
+        royalty: Number(input.royalty),
+        royalty_receiver: input.royaltyReceiver,
+        source_chain: input.sourceChain,
+        source_nft_contract_address: input.sourceNftContractAddress,
+        symbol: input.symbol,
+        token_amount: BigInt(input.tokenAmount),
+        token_id: input.tokenId,
+        transaction_hash: input.transactionHash,
+        lock_tx_chain: input.lockTxChain,
+      };
+    },
+    async approveNft(signer, tokenId, contract) {
+      const approve = await signer.functionCall({
+        contractId: contract,
+        methodName: "nft_approve",
+        args: {
+          token_id: tokenId,
+          account_id: bridge,
+          msg: null,
+        },
+        attachedDeposit: 0n,
+      });
+      return approve.transaction.hash;
+    },
+    async getClaimData(txHash) {
+      const receipts = await provider.connection.provider.txStatusReceipts(
+        txHash,
+        bridge,
+        "FINAL",
+      );
+      const log = receipts.receipts_outcome
+        .map((e) => e.outcome.logs)
+        .flatMap((e) => e)
+        .filter((e) => e.includes("locked"))[0];
+      if (!log) throw new Error("No Locked Event Found");
+      const parsed = JSON.parse(log).data;
+
+      const destinationChain = parsed.destination_chain;
+      const sourceChain = parsed.source_chain;
+      const tokenId = parsed.token_id;
+      const tokenAmount = parsed.token_amount.toString();
+
+      const fee = await storage.chainFee(destinationChain);
+      const royaltyReceiver = await storage.chainRoyalty(destinationChain);
+      let metadata: TNFTData = {
+        metadata: "",
+        name: "",
+        royalty: 0n,
+        symbol: "",
+      };
+      if (sourceChain === "NEAR") {
+        metadata = await this.nftData(
+          tokenId,
+          parsed.source_nft_contract_address,
+        );
+      }
+
+      const imgUri = (await fetchHttpOrIpfs(metadata.metadata)).image;
+
+      return {
+        destinationChain,
+        destinationUserAddress: parsed.destination_user_address,
+        tokenAmount,
+        tokenId,
+        nftType: parsed.nft_type,
+        sourceNftContractAddress: parsed.source_nft_contract_address,
+        sourceChain,
+        transactionHash: txHash,
+        fee: fee.toString(),
+        royaltyReceiver,
+        metadata: metadata.metadata,
+        name: metadata.name,
+        symbol: metadata.symbol,
+        royalty: metadata.royalty.toString(),
+        lockTxChain: identifier,
+        imgUri: imgUri,
+      };
+    },
+    async lockNft(
+      signer,
+      sourceNft,
+      destinationChain,
+      to,
+      tokenId,
+      metadata_uri,
+    ) {
+      const locked = await signer.functionCall({
+        contractId: bridge,
+        methodName: "lock_nft",
+        args: {
+          source_nft_contract_address: sourceNft,
+          destination_chain: destinationChain,
+          destination_user_address: to,
+          token_id: tokenId,
+          metadata_uri,
+        },
+      });
+      return {
+        ret: locked,
+        hash() {
+          return locked.transaction.hash;
+        },
+      };
+    },
+    async claimNft(signer, claimData, sig) {
+      const claimed = await signer.functionCall({
+        contractId: bridge,
+        methodName: "claim_nft",
+        args: {
+          cd: claimData,
+          signatures: sig.map((e) => {
+            return {
+              signer: e.signerAddress.replace("0x", ""),
+              signature: [...Buffer.from(e.signature.replace("0x", ""), "hex")],
+            };
+          }),
+        },
+        attachedDeposit: BigInt(claimData.fee),
+      });
+      return { hash: () => claimed.transaction.hash, ret: claimed };
+    },
+    async getBalance(signer, _) {
+      const ab = await signer.getAccountBalance();
+      return BigInt(ab.available);
+    },
+  };
+}
---
+++ src/handlers/near/types.ts
@@ -0,0 +1,64 @@
+import { Account, Near } from "near-api-js";
+import { FinalExecutionOutcome } from "near-api-js/lib/providers";
+import { BridgeStorage } from "../../contractsTypes/evm";
+import {
+  DeployNFTCollection,
+  MintNft,
+  ReadClaimed721Event,
+  TSingularNftChain,
+} from "../types";
+
+export type TNearClaimArgs = {
+  token_id: string;
+  source_chain: string;
+  destination_chain: string;
+  destination_user_address: string;
+  source_nft_contract_address: string;
+  name: string;
+  symbol: string;
+  royalty: number;
+  royalty_receiver: string;
+  metadata: string;
+  transaction_hash: string;
+  token_amount: bigint;
+  nft_type: string;
+  fee: string;
+  lock_tx_chain: string;
+};
+
+/**
+ * arguments required to issue an NFT
+ */
+export type NftIssueArgs = {
+  readonly contract: string;
+  readonly owner: string;
+  readonly token_id: string;
+  readonly title?: string;
+  readonly description?: string;
+  readonly uri: string;
+};
+
+export type TNearHandler = TSingularNftChain<
+  Account,
+  TNearClaimArgs,
+  unknown,
+  FinalExecutionOutcome,
+  Near
+> &
+  MintNft<Account, NftIssueArgs, { gasLimit: number; value: number }, string> &
+  DeployNFTCollection<
+    Account,
+    { name: string; ticker: string },
+    { gasLimit: number },
+    string
+  > &
+  ReadClaimed721Event;
+
+export type TNearParams = {
+  provider: Near;
+  gatewayURL: string;
+  bridge: string;
+  storage: BridgeStorage;
+  chainId: string;
+  identifier: string;
+};
