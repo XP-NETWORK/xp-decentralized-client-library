9f1614d037fce5bb0e2f6a10df08a86b75ff82e9
D4mph1r <102529837+D4mph1r@users.noreply.github.com>
2024-09-18 12:42:56 +0000

    Build: (4b136ce) chore(tezos): mainnet contract

--- dist/factory/config.d.ts
+++ dist/factory/config.d.ts
@@ -110,6 +110,13 @@ export declare namespace ChainFactoryConfigs {
             client: TonClient;
             storage: import("../contractsTypes/evm").BridgeStorage;
         };
+        tezosParams: {
+            bridge: string;
+            identifier: string;
+            storage: import("../contractsTypes/evm").BridgeStorage;
+            Tezos: TezosToolkit;
+            tzktApi: string;
+        };
     };
 }
 //# sourceMappingURL=config.d.ts.map
