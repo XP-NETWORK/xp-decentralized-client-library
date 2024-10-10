4cbdf7fc485226ce8de6d5bf2af7b0ac497fc061
D4mph1r <102529837+D4mph1r@users.noreply.github.com>
2024-10-10 21:52:04 +0000

    Build: (88105d5) chore(mx): new testnet contract

--- dist/factory/config.js
+++ dist/factory/config.js
@@ -68,7 +68,7 @@ var ChainFactoryConfigs;
                 tzktApi: "https://api.ghostnet.tzkt.io/",
             },
             multiversxParams: {
-                bridge: "erd1qqqqqqqqqqqqqpgqrv5m862qksnnuemlspxvgpvwvx5tsxl7txsq6xryjk",
+                bridge: "erd1qqqqqqqqqqqqqpgq8k2639tp7rrmm90f4254szf0uk6wm3agtxsq3nfuqy",
                 chainId: "D",
                 identifier: "MULTIVERSX",
                 gatewayURL: "https://devnet-gateway.multiversx.com",
